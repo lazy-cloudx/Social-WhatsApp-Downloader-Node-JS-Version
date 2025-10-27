087e6624ed87450872187a1b5ce36f3c1d13af0f
Laxy Cloud ☁️ <abhijitdas1519@gmail.com>
2025-10-27 17:21:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
   PINTEREST_API_BASE: 'https://wadownloader.amitdas.site/api/',
-  WHATSAPP_INSTANCE_ID: '68FF5AAE093E2x',
+  WHATSAPP_INSTANCE_ID: '68FF5AAE093E2',
   WHATSAPP_ACCESS_TOKEN: '68fc7b98bd564'
 };
